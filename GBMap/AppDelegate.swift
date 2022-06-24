935a141d451b1028649871cc8bfc4e297eff9dee
Spote777 <66115092+Spote777@users.noreply.github.com>
2022-06-24 15:36:16 +0300

    lesson4

--- GBMap/AppDelegate.swift
+++ GBMap/AppDelegate.swift
@@ -10,20 +10,20 @@ import GoogleMaps
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         GMSServices.provideAPIKey("AIzaSyAraL3zd7rlTheyZL7EIv868QwI1o2PVAs")
         return true
     }
-
+    
     // MARK: UISceneSession Lifecycle
-
+    
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-
+        
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
-
+    
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-
+        
     }
 }
